f9a8a8be18823d33caa8c010fd8ae7c151551605
Hemant <hsgaikwad0015@gmail.com>
2023-10-21 11:47:22 +0530

    minor changes

--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -1,53 +1,64 @@
-// 0e6185a48ea94c35978d28f4b38f8750
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
-import NewsArticle from '../../Component/Newsarticle/newsarticle';
-import "./Home.css"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import NewsArticle from './../../Component/NewsArticles/NewsArticle';
+import './Home.css';
 
 function Home() {
   const [news, setNews] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("pune")
+  const [searchQuery, setSearchQuery] = useState('pune');
 
   const loadNews = async () => {
     try {
-      const respopnse = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2023-10-16&to=2023-10-16&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`);
+      const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2023-10-16&to=2023-10-16&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`);
 
-      setNews(respopnse.data.article)
-    }
-    catch (error) {
-      console.log(Error)
+      setNews(response.data.articles);
+    } catch (error) {
+      console.log(error);
     }
   }
 
   useEffect(() => {
-    loadNews()
-  }, [])
+    loadNews();
+  }, []);
 
   useEffect(() => {
-    loadNews()
-  }, [searchQuery])
+    loadNews();
+  }, [searchQuery]);
 
   return (
     <div>
       <h1>News App</h1>
 
-      <input className='search-input'
+      <input
+        className='search-input'
         type="text"
         value={searchQuery}
         onChange={(e) => {
-          setSearchQuery(e.target.value)
-        }} />
+          setSearchQuery(e.target.value);
+        }}
+      />
+
       <div className='news-container'>
         {
           news.map((newsArticle, index) => {
-            const { author, title, description, url, urlToImage, publishedAt } = newsArticle
+            const { author, title, description, url, urlToImage, publishedAt } = newsArticle;
 
-            return (<NewsArticle author={author} title={title} description={description} url={url} urlToImage={urlToImage} publishedAt={publishedAt} key={index}/>)
+            return (
+              <NewsArticle
+                author={author}
+                title={title}
+                description={description}
+                url={url}
+                urlToImage={urlToImage}
+                publishedAt={publishedAt}
+                key={index}
+              />
+            );
           })
         }
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
